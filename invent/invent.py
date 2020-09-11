6d3b5dfcfc59126508cc40642d5c17a82e1f0027
spencer barriball <spencer.barriball@pwc.com>
2020-09-11 15:04:36 +0100

    add

---
+++ invent/invent.py
@@ -0,0 +1,9 @@
+
+items = [{'id': 1, 'name': 'laptop', 'value': 1000},
+         {'id': 2, 'name': 'chair', 'value': 300},
+         {'id': 3, 'name': 'book', 'value': 20}]
+
+
+def duplicate_items(items):
+    """ look at tests and write code to make them pass - hint: use a copying library that goes deep enough"""
+    pass
